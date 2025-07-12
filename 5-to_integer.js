3b1a3bde007a243c1c85addcefc24d25bbebc229
Eugene-Maina <mainaeugene23@gmail.com>
2025-07-12 22:50:00 +0300

    converts an argument to an interger

---
+++ 5-to_integer.js
@@ -0,0 +1,10 @@
+const convert=function(number){
+    if (number = Number(number)) {
+        console.log(`'My number:' number`);
+    }
+    else {
+        console.log('Not a number');
+    }
+    
+    
+}
