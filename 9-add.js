f365fd173ed9a99595cee696ce4554f353eb4966
Eugene-Maina <mainaeugene23@gmail.com>
2025-07-13 13:24:26 +0300

    made changes to the add function

--- 9-add.js
+++ 9-add.js
@@ -1,4 +1,11 @@
-const add= function(a,b){
+/*const add= function(a,b){
     return a + b;};
 const calcAddition=add();
-console.log(calcAddition);
+console.log(calcAddition);*/
+const add = function(a, b) {
+    return a + b;
+};
+const args = process.argv.slice(2);
+const first = parseInt(args[0], 10);
+const second = parseInt(args[1], 10);
+console.log(add(first, second));
